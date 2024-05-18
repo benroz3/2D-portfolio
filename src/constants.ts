a0558bad5084f3a178a7bcc40805af503e78a075
benroz3 <108490385+benroz3@users.noreply.github.com>
2024-05-18 20:31:29 +0300

    fixed small typo

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const dialogueData = {
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to sit here and read books.`,
   tv: `That's my TV. I like to relax here and watch programming YouTube videos to stay up to date with the latest technologies.`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. Getting a full night sleep is important fot keep being productive as a programmer.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. Getting a full night sleep is important to keep being productive as a programmer.`,
   resume: `This is my desk and on it is my resume. You can find a brief of it on my LinkedIn page - <a href="https://www.linkedin.com/in/ben-rozen/" target="_blank">Feel free to check it out!</a>`,
   projects: `All my projects are in my GitHub account. You can find my most interesting projects on my portfolio page <a href="https://ben-rozen-portfolio.netlify.app/" target="_blank">here</a>.`,
   library: `There are a lot books on my shelf. I try to read everyday to expand my knowledge.`,
